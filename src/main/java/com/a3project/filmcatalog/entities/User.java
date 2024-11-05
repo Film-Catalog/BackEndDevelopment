a33989c6cb399a959e9fbba963475233c69c81e7
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-11-05 08:26:57 -0300

    code refactoring

--- src/main/java/com/a3project/filmcatalog/entities/User.java
+++ src/main/java/com/a3project/filmcatalog/entities/User.java
@@ -3,27 +3,22 @@ package com.a3project.filmcatalog.entities;
 public class User {
 
 	private Long id;
-	private String username;
 	private String email;
 
 	public User() {
 	}
 
-	public User(String username, String email) {
-		this.username = username;
+	public User(Long id, String email) {
+		this.id = id;
 		this.email = email;
 	}
 
 	public Long getId() {
 		return id;
 	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
+	
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getEmail() {
